1326c27f004ed7a4f4536b7de51406be25d2bfa5
njucz <zhcheng@microsoft.com>
2020-07-02 16:32:56 +0800

    update

--- internal/terraform/rootmodule/utils.go
+++ internal/terraform/rootmodule/utils.go
@@ -8,5 +8,5 @@ import (
 func pathEquals(path1, path2 string) bool {
 	volumn1 := filepath.VolumeName(path1)
 	volumn2 := filepath.VolumeName(path2)
-	return strings.EqualFold(volumn1, volumn2) && path1[len(volumn1)] == path2[len(volumn2)]
+	return strings.EqualFold(volumn1, volumn2) && path1[len(volumn1):] == path2[len(volumn2):]
 }
