b5a949697a06624262b27c841e4882d55d95852b
jojo43 <s.jmr0124@gmail.com>
2020-07-04 16:46:28 +0900

    Pass -no-color to terraform

--- internal/terraform/rootmodule/root_module_manager_mock_test.go
+++ internal/terraform/rootmodule/root_module_manager_mock_test.go
@@ -27,11 +27,11 @@ func TestNewRootModuleManagerMock_mocks(t *testing.T) {
 				TerraformExecQueue: &exec.MockQueue{
 					Q: []*exec.MockItem{
 						{
-							Args:   []string{"version"},
+							Args:   []string{"version", "-no-color"},
 							Stdout: "Terraform v0.12.0\n",
 						},
 						{
-							Args:   []string{"providers", "schema", "-json"},
+							Args:   []string{"providers", "-no-color", "schema", "-json"},
 							Stdout: `{"format_version":"0.1"}` + "\n",
 						},
 					},
