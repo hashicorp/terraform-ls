4b0b1540cc74a19b877d1270bedcb58176e176be
James Pogran <jpogran@outlook.com>
2021-08-27 16:36:06 -0400

    Module View Command Handler
    Adds a handler to return a list of modules used by a given module path. Each module returned shows the module name, version, documentation link and what type of module it is (local, github, or terraform registry). It also detects if a module has nested modules, and adds them in the `DependentModules` property.
    
    This is meant to be used in tandem with https://github.com/hashicorp/vscode-terraform/pull/746
    
    TODO: It is technically incorrect to use the package hashicorp/terraform-registry-address here as it is written to parse Terraform provider addresses and may not work correctly on Terraform module addresses. The proper approach is to create a new parsing library that is dedicated to parsing these kinds of addresses correctly, by re-using the logic defined in the authorative source: hashicorp/terraform/internal/addrs/module_source.go. However this works enough for now to identify module types for display in vscode-terraform.
    
    This also increases the CI timeout for the build process. We think the introduction of go-getter inflated the dependency tree and adds more time. At the moment it is more economical to eat the build time than take the engineering time to copy the methods needed to detect the correct module sources.

---
+++ internal/langserver/handlers/command/module_calls_test.go
@@ -0,0 +1,89 @@
+package command
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform-ls/internal/terraform/datadir"
+)
+
+func Test_parseModuleRecords(t *testing.T) {
+	tests := []struct {
+		name    string
+		records []datadir.ModuleRecord
+		want    []moduleCall
+	}{
+		{
+			name: "detects terraform module types",
+			records: []datadir.ModuleRecord{
+				{
+					Key:        "ec2_instances",
+					SourceAddr: "terraform-aws-modules/ec2-instance/aws",
+					VersionStr: "2.12.0",
+					Dir:        ".terraform\\modules\\ec2_instances",
+				},
+				{
+					Key:        "web_server_sg",
+					SourceAddr: "github.com/terraform-aws-modules/terraform-aws-security-group",
+					VersionStr: "",
+					Dir:        ".terraform\\modules\\web_server_sg",
+				},
+				{
+					Key:        "eks",
+					SourceAddr: "terraform-aws-modules/eks/aws",
+					VersionStr: "17.20.0",
+					Dir:        ".terraform\\modules\\eks",
+				},
+				{
+					Key:        "eks.fargate",
+					SourceAddr: "./modules/fargate",
+					VersionStr: "",
+					Dir:        ".terraform\\modules\\eks\\modules\\fargate",
+				},
+			},
+			want: []moduleCall{
+				{
+					Name:             "ec2_instances",
+					SourceAddr:       "terraform-aws-modules/ec2-instance/aws",
+					Version:          "2.12.0",
+					SourceType:       "tfregistry",
+					DocsLink:         "https://registry.terraform.io/modules/terraform-aws-modules/ec2-instance/aws/2.12.0",
+					DependentModules: []moduleCall{},
+				},
+				{
+					Name:       "eks",
+					SourceAddr: "terraform-aws-modules/eks/aws",
+					Version:    "17.20.0",
+					SourceType: "tfregistry",
+					DocsLink:   "https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/17.20.0",
+					DependentModules: []moduleCall{
+						{
+							Name:             "fargate",
+							SourceAddr:       "./modules/fargate",
+							Version:          "",
+							SourceType:       "local",
+							DocsLink:         "",
+							DependentModules: []moduleCall{},
+						},
+					},
+				},
+				{
+					Name:             "web_server_sg",
+					SourceAddr:       "github.com/terraform-aws-modules/terraform-aws-security-group",
+					Version:          "",
+					SourceType:       "github",
+					DocsLink:         "",
+					DependentModules: []moduleCall{},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := parseModuleRecords(tt.records)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Fatalf("module mismatch: %s", diff)
+			}
+		})
+	}
+}
