75a5230dfd0b5fe6dbbdc9055e55cb6951ebf79e
Radek Simko <radek.simko@gmail.com>
2021-11-10 09:34:58 +0000

    telemetry: Disable logging when telemetry is disabled (#706)

--- internal/telemetry/noop.go
+++ internal/telemetry/noop.go
@@ -17,6 +17,4 @@ func (t *NoopSender) log() *log.Logger {
 	return log.New(ioutil.Discard, "", 0)
 }
 
-func (t *NoopSender) SendEvent(ctx context.Context, name string, properties map[string]interface{}) {
-	t.log().Printf("telemetry disabled %q: %#v", name, properties)
-}
+func (t *NoopSender) SendEvent(ctx context.Context, name string, properties map[string]interface{}) {}
