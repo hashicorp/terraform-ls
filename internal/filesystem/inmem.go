c440238626f20c5bfe7778e616b5e756cc9983fb
Radek Simko <radek.simko@gmail.com>
2022-01-21 15:30:42 +0000

    internal/filesystem: reduce impl to simple OS FS + interface for DocumentStore
    Previously filesystem package had two major use cases, to offer a unified io/fs.FS interface for e.g. parsing *.tf or *.tfvars, which was implemented mostly via external library (spf13/afero). Secondly it also provided a direct full access to the "in-memory layer" of the filesystem for RPC handlers (e.g. didOpen, didChange, didClose, ...).
    
    These use cases rarely overlap throughout the codebase and so this lead to unnecessary imports of the `filesystem` package in places where we only needed either the OS-level FS or in-mem FS, but almost never both.
    
    This decoupling allows us to import `filesystem` or `state.DocumentStore` separately.
    
    Also, as we no longer need the in-mem backend of afero, it makes more sense to just reimplement the small part of the 3rd party library instead.

---
+++ internal/filesystem/inmem.go
@@ -0,0 +1,79 @@
+package filesystem
+
+import (
+	"bytes"
+	"io/fs"
+	"time"
+)
+
+type inMemFile struct {
+	bytes []byte
+	info  fs.FileInfo
+}
+
+func (f inMemFile) Read(b []byte) (int, error) {
+	return bytes.NewBuffer(f.bytes).Read(b)
+}
+
+func (f inMemFile) Stat() (fs.FileInfo, error) {
+	return f.info, nil
+}
+
+func (f inMemFile) Close() error {
+	return nil
+}
+
+type inMemFileInfo struct {
+	name    string
+	size    int
+	mode    fs.FileMode
+	modTime time.Time
+	isDir   bool
+}
+
+func (fi inMemFileInfo) Name() string {
+	return fi.name
+}
+
+func (fi inMemFileInfo) Size() int64 {
+	return int64(fi.size)
+}
+
+func (fi inMemFileInfo) Mode() fs.FileMode {
+	return fi.mode
+}
+
+func (fi inMemFileInfo) ModTime() time.Time {
+	return fi.modTime
+}
+
+func (fi inMemFileInfo) IsDir() bool {
+	return fi.isDir
+}
+
+func (fi inMemFileInfo) Sys() interface{} {
+	return nil
+}
+
+type inMemDirEntry struct {
+	name  string
+	isDir bool
+	typ   fs.FileMode
+	info  fs.FileInfo
+}
+
+func (de inMemDirEntry) Name() string {
+	return de.name
+}
+
+func (de inMemDirEntry) IsDir() bool {
+	return de.isDir
+}
+
+func (de inMemDirEntry) Type() fs.FileMode {
+	return de.typ
+}
+
+func (de inMemDirEntry) Info() (fs.FileInfo, error) {
+	return de.info, nil
+}
