51d07ef221f9662d8485f8ee97c7e4c47b7e09f5
Radek Simko <radek.simko@gmail.com>
2022-01-25 11:45:21 +0000

    internal/lsp: Update references

--- internal/lsp/position.go
+++ internal/lsp/position.go
@@ -2,53 +2,25 @@ package lsp
 
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/terraform-ls/internal/filesystem"
+	"github.com/hashicorp/terraform-ls/internal/document"
 	lsp "github.com/hashicorp/terraform-ls/internal/protocol"
 )
 
-type filePosition struct {
-	fh  *fileHandler
-	pos hcl.Pos
-}
-
-func (p *filePosition) Position() hcl.Pos {
-	return p.pos
-}
-
-func (p *filePosition) URI() string {
-	return p.fh.URI()
-}
-
-func (p *filePosition) FullPath() string {
-	return p.fh.FullPath()
-}
-
-func (p *filePosition) Dir() string {
-	return p.fh.Dir()
-}
-
-func (p *filePosition) Filename() string {
-	return p.fh.Filename()
-}
-
-func FilePositionFromDocumentPosition(params lsp.TextDocumentPositionParams, f File) (*filePosition, error) {
-	byteOffset, err := filesystem.ByteOffsetForPos(f.Lines(), lspPosToFsPos(params.Position))
+func HCLPositionFromLspPosition(pos lsp.Position, doc *document.Document) (hcl.Pos, error) {
+	byteOffset, err := document.ByteOffsetForPos(doc.Lines, lspPosToDocumentPos(pos))
 	if err != nil {
-		return nil, err
+		return hcl.Pos{}, err
 	}
 
-	return &filePosition{
-		fh: FileHandlerFromDocumentURI(params.TextDocument.URI),
-		pos: hcl.Pos{
-			Line:   int(params.Position.Line) + 1,
-			Column: int(params.Position.Character) + 1,
-			Byte:   byteOffset,
-		},
+	return hcl.Pos{
+		Line:   int(pos.Line) + 1,
+		Column: int(pos.Character) + 1,
+		Byte:   byteOffset,
 	}, nil
 }
 
-func lspPosToFsPos(pos lsp.Position) filesystem.Pos {
-	return filesystem.Pos{
+func lspPosToDocumentPos(pos lsp.Position) document.Pos {
+	return document.Pos{
 		Line:   int(pos.Line),
 		Column: int(pos.Character),
 	}
