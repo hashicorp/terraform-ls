942456f4343a7330215120a3025cbd1d21e84a05
Radek Simko <radek.simko@gmail.com>
2022-02-17 21:01:05 +0000

    internal/uri: refactor & add comments

--- internal/uri/uri.go
+++ internal/uri/uri.go
@@ -3,20 +3,55 @@ package uri
 import (
 	"fmt"
 	"net/url"
+	"os"
 	"path/filepath"
+	"strings"
+	"unicode"
 )
 
-func FromPath(path string) string {
-	p := filepath.ToSlash(path)
-	p = wrapPath(p)
+// FromPath creates a URI from OS-specific path per RFC 8089 "file" URI Scheme
+func FromPath(rawPath string) string {
+	// Cleaning up path trims any trailing separator
+	// which then (in the context of URI below) complies
+	// with RFC 3986 § 6.2.4 which is relevant in LSP.
+	path := filepath.Clean(rawPath)
+
+	// Convert any OS-specific separators to '/'
+	path = filepath.ToSlash(path)
+
+	volume := filepath.VolumeName(rawPath)
+	if isWindowsDriveVolume(volume) {
+		// Per RFC 8089 (Appendix F. Collected Nonstandard Rules)
+		// file-absolute = "/" drive-letter path-absolute
+		// i.e. paths with drive-letters (such as C:) are prepended
+		// with an additional slash.
+		path = "/" + path
+	}
 
 	u := &url.URL{
 		Scheme: "file",
-		Path:   p,
+		Path:   path,
 	}
+
+	// Ensure that String() returns uniform escaped path at all times
+	escapedPath := u.EscapedPath()
+	if escapedPath != path {
+		u.RawPath = escapedPath
+	}
+
 	return u.String()
 }
 
+// isWindowsDriveVolume returns true if the volume name has a drive letter.
+// For example: C:\example.
+func isWindowsDriveVolume(path string) bool {
+	if len(path) < 2 {
+		return false
+	}
+	return unicode.IsLetter(rune(path[0])) && path[1] == ':'
+}
+
+// IsURIValid checks whether uri is a valid URI per RFC 8089
 func IsURIValid(uri string) bool {
 	_, err := parseUri(uri)
 	if err != nil {
@@ -26,24 +61,72 @@ func IsURIValid(uri string) bool {
 	return true
 }
 
-func mustParseUri(uri string) string {
-	u, err := parseUri(uri)
-	if err != nil {
-		panic(fmt.Sprintf("invalid URI: %s", uri))
-	}
-	return u
-}
-
-func parseUri(uri string) (string, error) {
-	u, err := url.ParseRequestURI(uri)
+// PathFromURI extracts OS-specific path from an RFC 8089 "file" URI Scheme
+func PathFromURI(rawUri string) (string, error) {
+	uri, err := parseUri(rawUri)
 	if err != nil {
 		return "", err
 	}
 
-	if u.Scheme != "file" {
-		return "", fmt.Errorf("unexpected scheme %q in URI %q",
-			u.Scheme, uri)
+	// Convert '/' to any OS-specific separators
+	osPath := filepath.FromSlash(uri.Path)
+
+	// Upstream net/url parser prefers consistency and reusability
+	// (e.g. in HTTP servers) which complies with
+	// the Comparison Ladder as defined in § 6.2 of RFC 3968.
+	// https://datatracker.ietf.org/doc/html/rfc3986#section-6.2
+	//
+	// Cleaning up path trims any trailing separator
+	// which then still complies with RFC 3986 per § 6.2.4
+	// which is relevant in LSP.
+	osPath = filepath.Clean(osPath)
+
+	trimmedOsPath := trimLeftPathSeparator(osPath)
+	if strings.HasSuffix(filepath.VolumeName(trimmedOsPath), ":") {
+		// Per RFC 8089 (Appendix F. Collected Nonstandard Rules)
+		// file-absolute = "/" drive-letter path-absolute
+		// i.e. paths with drive-letters (such as C:) are preprended
+		// with an additional slash (which we converted to OS separator above)
+		// which we trim here.
+		// See also https://github.com/golang/go/issues/6027
+		osPath = trimmedOsPath
 	}
 
-	return url.PathUnescape(u.Path)
+	return osPath, nil
+}
+
+func trimLeftPathSeparator(s string) string {
+	return strings.TrimLeftFunc(s, func(r rune) bool {
+		return r == os.PathSeparator
+	})
+}
+
+func MustPathFromURI(uri string) string {
+	osPath, err := PathFromURI(uri)
+	if err != nil {
+		panic(fmt.Sprintf("invalid URI: %s", uri))
+	}
+	return osPath
+}
+
+func parseUri(rawUri string) (*url.URL, error) {
+	uri, err := url.ParseRequestURI(rawUri)
+	if err != nil {
+		return nil, err
+	}
+
+	if uri.Scheme != "file" {
+		return nil, fmt.Errorf("unexpected scheme %q in URI %q",
+			uri.Scheme, rawUri)
+	}
+
+	// Upstream net/url parser prefers consistency and reusability
+	// (e.g. in HTTP servers) which complies with
+	// the Comparison Ladder as defined in § 6.2 of RFC 3968.
+	// https://datatracker.ietf.org/doc/html/rfc3986#section-6.2
+	// Here we essentially just implement § 6.2.4
+	// as it is relevant in LSP (which uses the file scheme).
+	uri.Path = strings.TrimSuffix(uri.Path, "/")
+
+	return uri, nil
 }
--- internal/uri/uri_test.go
+++ internal/uri/uri_test.go
@@ -1,6 +1,8 @@
 package uri
 
 import (
+	"fmt"
+	"runtime"
 	"testing"
 )
 
@@ -8,5 +10,119 @@ func TestIsURIValid_invalid(t *testing.T) {
 	uri := "output:extension-output-%232"
 	if IsURIValid(uri) {
 		t.Fatalf("Expected %q to be invalid", uri)
+	}
+}
+
+func TestFromPath(t *testing.T) {
+	type testCase struct {
+		RawPath     string
+		ExpectedURI string
+	}
+	testCases := []testCase{}
+
+	if runtime.GOOS == "windows" {
+		// windows
+		testCases = []testCase{
+			{
+				RawPath:     `C:\Users\With Space\file.tf`,
+				ExpectedURI: "file:///C:/Users/With%20Space/file.tf",
+			},
+			{
+				RawPath:     `C:\Users\WithoutSpace\file.tf`,
+				ExpectedURI: "file:///C:/Users/WithoutSpace/file.tf",
+			},
+			{
+				RawPath:     `C:\Users\TrailingSeparator\`,
+				ExpectedURI: "file:///C:/Users/TrailingSeparator",
+			},
+		}
+	} else {
+		// unix
+		testCases = []testCase{
+			{
+				RawPath:     "/random/path/with space",
+				ExpectedURI: "file:///random/path/with%20space",
+			},
+			{
+				RawPath:     "/random/path",
+				ExpectedURI: "file:///random/path",
+			},
+			{
+				RawPath:     `/path/with/trailing-separator/`,
+				ExpectedURI: "file:///path/with/trailing-separator",
+			},
+		}
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			uri := FromPath(tc.RawPath)
+			if uri != tc.ExpectedURI {
+				t.Fatalf("URI doesn't match.\nExpected: %q\nGiven: %q",
+					tc.ExpectedURI, uri)
+			}
+		})
+	}
+}
+
+func TestPathFromURI(t *testing.T) {
+	type testCase struct {
+		URI          string
+		ExpectedPath string
+	}
+	testCases := []testCase{}
+
+	if runtime.GOOS == "windows" {
+		// windows
+		testCases = []testCase{
+			{
+				URI:          "file:///C:/Users/With%20Space/tf-test/file.tf",
+				ExpectedPath: `C:\Users\With Space\tf-test\file.tf`,
+			},
+			{
+				URI:          "file:///C:/Users/With%20Space/tf-test",
+				ExpectedPath: `C:\Users\With Space\tf-test`,
+			},
+			// Ensure URI with trailing slash is trimmed per RFC 3986 § 6.2.4
+			{
+				URI:          "file:///C:/Users/Test/tf-test/",
+				ExpectedPath: `C:\Users\Test\tf-test`,
+			},
+		}
+	} else {
+		// unix
+		testCases = []testCase{
+			{
+				URI:          "file:///valid/path/to/file.tf",
+				ExpectedPath: "/valid/path/to/file.tf",
+			},
+			{
+				URI:          "file:///valid/path/to",
+				ExpectedPath: "/valid/path/to",
+			},
+
+			// Ensure URI with trailing slash is trimmed per RFC 3986 § 6.2.4
+			{
+				URI:          "file:///random/dir/",
+				ExpectedPath: "/random/dir",
+			},
+		}
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			if !IsURIValid(tc.URI) {
+				t.Fatalf("Expected %q to be valid", tc.URI)
+			}
+
+			path, err := PathFromURI(tc.URI)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if path != tc.ExpectedPath {
+				t.Fatalf("Expected full path: %q, given: %q",
+					tc.ExpectedPath, path)
+			}
+		})
 	}
 }
