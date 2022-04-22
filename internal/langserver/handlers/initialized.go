d4167b41b754dac8d9ebedcffd4b16bc7a3c1d20
Radek Simko <radek.simko@gmail.com>
2022-04-22 13:16:20 +0100

    WIP

--- internal/langserver/handlers/initialized.go
+++ internal/langserver/handlers/initialized.go
@@ -3,9 +3,32 @@ package handlers
 import (
 	"context"
 
+	"github.com/creachadair/jrpc2"
 	lsp "github.com/hashicorp/terraform-ls/internal/protocol"
 )
 
 func Initialized(ctx context.Context, params lsp.InitializedParams) error {
+
+	// dynamic registraion
+
+	jrpc2.ServerFromContext(ctx).Callback(ctx, "client/registerCapability", lsp.RegistrationParams{
+		Registrations: []lsp.Registration{
+			{
+				ID:     "79eee87c-c409-4664-8102-e03263673f6f",
+				Method: "workspace/didChangeWatchedFiles",
+				RegisterOptions: lsp.DidChangeWatchedFilesRegistrationOptions{
+					Watchers: []lsp.FileSystemWatcher{
+						{
+							GlobPattern: "**/*.tf",
+						},
+						{
+							GlobPattern: "**/*.tfvars",
+						},
+					},
+				},
+			},
+		},
+	})
+
 	return nil
 }
