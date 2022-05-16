fb717436a6f48adc58946b80cc9ad8cfb0338cb1
Radek Simko <radek.simko@gmail.com>
2022-05-16 20:13:46 +0100

    account for file/dir deletions and creations too

--- internal/langserver/handlers/did_change_watched_files.go
+++ internal/langserver/handlers/did_change_watched_files.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/creachadair/jrpc2"
 	"github.com/hashicorp/terraform-ls/internal/document"
@@ -20,47 +21,111 @@ func (svc *service) DidChangeWatchedFiles(ctx context.Context, params lsp.DidCha
 	for _, change := range params.Changes {
 		rawURI := string(change.URI)
 
-		fullPath, err := uri.PathFromURI(rawURI)
+		rawPath, err := uri.PathFromURI(rawURI)
 		if err != nil {
-			svc.logger.Printf("Unable to update %q: %s", rawURI, err)
+			svc.logger.Printf("error parsing %q: %s", rawURI, err)
 			continue
 		}
 
-		fi, err := os.Stat(fullPath)
-		if err != nil {
-			svc.logger.Printf("Unable to update %q: %s ", fullPath, err)
-			continue
-		}
-
-		// URI can either be a file or a directory based on the LSP spec.
-		var dirHandle document.DirHandle
-		if !fi.IsDir() {
-			if !ast.IsModuleFilename(fi.Name()) && !ast.IsVarsFilename(fi.Name()) {
-				jrpc2.ServerFromContext(ctx).Notify(ctx, "window/showMessage", &lsp.ShowMessageParams{
-					Type: lsp.Warning,
-					Message: fmt.Sprintf("Unable to update %q: filetype not supported. "+
-						"This is likely a bug which should be reported.", fullPath),
-				})
+		if change.Type == protocol.Deleted {
+			// We don't know whether file or dir is being deleted
+			// 1st we just blindly try to look it up as a directory
+			_, err = svc.modStore.ModuleByPath(rawPath)
+			if err == nil {
+				svc.removeIndexedModule(ctx, rawURI)
 				continue
 			}
-			docHandle := document.HandleFromPath(fullPath)
-			dirHandle = docHandle.Dir
-		} else {
-			dirHandle = document.DirHandleFromPath(fullPath)
-		}
 
-		if change.Type == protocol.Changed {
-			_, err = svc.modStore.ModuleByPath(dirHandle.Path())
+			// 2nd we try again assuming it is a file
+			parentDir := path.Dir(rawPath)
+			_, err = svc.modStore.ModuleByPath(parentDir)
 			if err != nil {
+				svc.logger.Printf("error finding module (%q deleted): %s", parentDir, err)
 				continue
 			}
 
+			// and check the parent directory still exists
+			fi, err := os.Stat(parentDir)
+			if err != nil {
+				if os.IsNotExist(err) {
+					// if not, we remove the indexed module
+					svc.removeIndexedModule(ctx, rawURI)
+					continue
+				}
+				svc.logger.Printf("error checking existence (%q deleted): %s", parentDir, err)
+				continue
+			}
+			if !fi.IsDir() {
+				svc.logger.Printf("error: %q (deleted) is not a directory", parentDir)
+				continue
+			}
+
+			// if the parent directory exists, we just need to
+			// reparse the module after a file was deleted from it
+			dirHandle := document.DirHandleFromPath(parentDir)
 			jobIds, err := svc.parseAndDecodeModule(dirHandle)
 			if err != nil {
+				svc.logger.Printf("error parsing module (%q deleted): %s", rawURI, err)
 				continue
 			}
 
 			ids = append(ids, jobIds...)
+		}
+
+		if change.Type == protocol.Changed {
+			ph, err := modHandleFromRawOsPath(ctx, rawPath)
+			if err != nil {
+				if err == ErrorSkip {
+					continue
+				}
+				svc.logger.Printf("error (%q changed): %s", rawURI, err)
+				continue
+			}
+
+			_, err = svc.modStore.ModuleByPath(ph.DirHandle.Path())
+			if err != nil {
+				svc.logger.Printf("error finding module (%q changed): %s", rawURI, err)
+				continue
+			}
+
+			jobIds, err := svc.parseAndDecodeModule(ph.DirHandle)
+			if err != nil {
+				svc.logger.Printf("error parsing module (%q changed): %s", rawURI, err)
+				continue
+			}
+
+			ids = append(ids, jobIds...)
+		}
+
+		if change.Type == protocol.Created {
+			ph, err := modHandleFromRawOsPath(ctx, rawPath)
+			if err != nil {
+				if err == ErrorSkip {
+					continue
+				}
+				svc.logger.Printf("error (%q created): %s", rawURI, err)
+				continue
+			}
+
+			if ph.IsDir {
+				err = svc.stateStore.WalkerPaths.EnqueueDir(ph.DirHandle)
+				if err != nil {
+					jrpc2.ServerFromContext(ctx).Notify(ctx, "window/showMessage", &lsp.ShowMessageParams{
+						Type: lsp.Warning,
+						Message: fmt.Sprintf("Ignoring new folder %s: %s."+
+							" This is most likely bug, please report it.", rawURI, err),
+					})
+					continue
+				}
+			} else {
+				jobIds, err := svc.parseAndDecodeModule(ph.DirHandle)
+				if err != nil {
+					svc.logger.Printf("error parsing module (%q created): %s", rawURI, err)
+					continue
+				}
+
+				ids = append(ids, jobIds...)
+			}
 		}
 	}
 
@@ -70,4 +135,47 @@ func (svc *service) DidChangeWatchedFiles(ctx context.Context, params lsp.DidCha
 	}
 
 	return nil
+}
+
+func modHandleFromRawOsPath(ctx context.Context, rawPath string) (*parsedModuleHandle, error) {
+	fi, err := os.Stat(rawPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// URI can either be a file or a directory based on the LSP spec.
+	if fi.IsDir() {
+		return &parsedModuleHandle{
+			DirHandle: document.DirHandleFromPath(rawPath),
+			IsDir:     true,
+		}, nil
+	}
+
+	if !ast.IsModuleFilename(fi.Name()) && !ast.IsVarsFilename(fi.Name()) {
+		jrpc2.ServerFromContext(ctx).Notify(ctx, "window/showMessage", &lsp.ShowMessageParams{
+			Type: lsp.Warning,
+			Message: fmt.Sprintf("Unable to update %q: filetype not supported. "+
+				"This is likely a bug which should be reported.", rawPath),
+		})
+		return nil, ErrorSkip
+	}
+
+	docHandle := document.HandleFromPath(rawPath)
+	return &parsedModuleHandle{
+		DirHandle: docHandle.Dir,
+		IsDir:     false,
+	}, nil
+}
+
+type parsedModuleHandle struct {
+	DirHandle document.DirHandle
+	IsDir     bool
+}
+
+var ErrorSkip = errSkip{}
+
+type errSkip struct{}
+
+func (es errSkip) Error() string {
+	return "skip"
 }
--- internal/langserver/handlers/did_change_watched_files_test.go
+++ internal/langserver/handlers/did_change_watched_files_test.go
@@ -7,6 +7,9 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/go-version"
+	tfjson "github.com/hashicorp/terraform-json"
+	"github.com/hashicorp/terraform-ls/internal/document"
 	"github.com/hashicorp/terraform-ls/internal/langserver"
 	"github.com/hashicorp/terraform-ls/internal/state"
 	"github.com/hashicorp/terraform-ls/internal/terraform/exec"
@@ -14,7 +17,7 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-func TestLangServer_DidChangeWatchedFiles_file(t *testing.T) {
+func TestLangServer_DidChangeWatchedFiles_change_file(t *testing.T) {
 	tmpDir := TempDir(t)
 
 	InitPluginCache(t, tmpDir.Path())
@@ -74,7 +77,7 @@ func TestLangServer_DidChangeWatchedFiles_file(t *testing.T) {
 	}
 
 	// Change main.tf on disk
-	newSrc := `variable "original" {
+	newSrc := `variable "new" {
   default = "foo"
 }
 `
@@ -123,7 +126,254 @@ func TestLangServer_DidChangeWatchedFiles_file(t *testing.T) {
 	}
 }
 
-func TestLangServer_DidChangeWatchedFiles_dir(t *testing.T) {
+func TestLangServer_DidChangeWatchedFiles_create_file(t *testing.T) {
+	tmpDir := TempDir(t)
+
+	InitPluginCache(t, tmpDir.Path())
+
+	originalSrc := `variable "original" {
+  default = "foo"
+}
+`
+	err := os.WriteFile(filepath.Join(tmpDir.Path(), "main.tf"), []byte(originalSrc), 0o755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ss, err := state.NewStateStore()
+	if err != nil {
+		t.Fatal(err)
+	}
+	wc := module.NewWalkerCollector()
+
+	ls := langserver.NewLangServerMock(t, NewMockSession(&MockSessionInput{
+		TerraformCalls: &exec.TerraformMockCalls{
+			PerWorkDir: map[string][]*mock.Call{
+				tmpDir.Path(): {
+					{
+						Method:        "Version",
+						Repeatability: 2,
+						Arguments: []interface{}{
+							mock.AnythingOfType(""),
+						},
+						ReturnArguments: []interface{}{
+							version.Must(version.NewVersion("0.12.0")),
+							nil,
+							nil,
+						},
+					},
+					{
+						Method:        "GetExecPath",
+						Repeatability: 1,
+						ReturnArguments: []interface{}{
+							"",
+						},
+					},
+					{
+						Method:        "ProviderSchemas",
+						Repeatability: 2,
+						Arguments: []interface{}{
+							mock.AnythingOfType(""),
+						},
+						ReturnArguments: []interface{}{
+							&tfjson.ProviderSchemas{
+								FormatVersion: "0.1",
+								Schemas: map[string]*tfjson.ProviderSchema{
+									"test": {
+										ConfigSchema: &tfjson.Schema{},
+									},
+								},
+							},
+							nil,
+						},
+					},
+				},
+			},
+		},
+		StateStore:      ss,
+		WalkerCollector: wc,
+	}))
+	stop := ls.Start(t)
+	defer stop()
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "initialize",
+		ReqParams: fmt.Sprintf(`{
+	    "capabilities": {},
+	    "rootUri": %q,
+	    "processId": 12345
+	}`, tmpDir.URI)})
+	waitForWalkerPath(t, ss, wc, tmpDir)
+	ls.Notify(t, &langserver.CallRequest{
+		Method:    "initialized",
+		ReqParams: "{}",
+	})
+
+	// Verify main.tf was parsed
+	mod, err := ss.Modules.ModuleByPath(tmpDir.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedFiles := mod.ParsedModuleFiles.AsMap()
+	parsedFile, ok := parsedFiles["main.tf"]
+	if !ok {
+		t.Fatalf("file not parsed: %q", "main.tf")
+	}
+	if diff := cmp.Diff(originalSrc, string(parsedFile.Bytes)); diff != "" {
+		t.Fatalf("bytes mismatch for %q: %s", "main.tf", diff)
+	}
+
+	// Create another.tf on disk
+	newSrc := `variable "another" {
+  default = "foo"
+}
+`
+	err = os.WriteFile(filepath.Join(tmpDir.Path(), "another.tf"), []byte(newSrc), 0o755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Verify another.tf was not parsed *yet*
+	mod, err = ss.Modules.ModuleByPath(tmpDir.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedFiles = mod.ParsedModuleFiles.AsMap()
+	parsedFile, ok = parsedFiles["another.tf"]
+	if ok {
+		t.Fatalf("not expected to be parsed: %q", "another.tf")
+	}
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "workspace/didChangeWatchedFiles",
+		ReqParams: fmt.Sprintf(`{
+    "changes": [
+        {
+            "uri": "%s/main.tf",
+            "type": 1
+        }
+    ]
+}`, TempDir(t).URI)})
+	waitForWalkerPath(t, ss, wc, tmpDir)
+
+	// Verify another.tf was parsed
+	mod, err = ss.Modules.ModuleByPath(tmpDir.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedFiles = mod.ParsedModuleFiles.AsMap()
+	parsedFile, ok = parsedFiles["another.tf"]
+	if !ok {
+		t.Fatalf("file not parsed: %q", "another.tf")
+	}
+	if diff := cmp.Diff(newSrc, string(parsedFile.Bytes)); diff != "" {
+		t.Fatalf("bytes mismatch for %q: %s", "another.tf", diff)
+	}
+}
+
+func TestLangServer_DidChangeWatchedFiles_delete_file(t *testing.T) {
+	tmpDir := TempDir(t)
+
+	InitPluginCache(t, tmpDir.Path())
+
+	originalSrc := `variable "original" {
+  default = "foo"
+}
+`
+	err := os.WriteFile(filepath.Join(tmpDir.Path(), "main.tf"), []byte(originalSrc), 0o755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ss, err := state.NewStateStore()
+	if err != nil {
+		t.Fatal(err)
+	}
+	wc := module.NewWalkerCollector()
+
+	ls := langserver.NewLangServerMock(t, NewMockSession(&MockSessionInput{
+		TerraformCalls: &exec.TerraformMockCalls{
+			PerWorkDir: map[string][]*mock.Call{
+				tmpDir.Path(): validTfMockCalls(),
+			},
+		},
+		StateStore:      ss,
+		WalkerCollector: wc,
+	}))
+	stop := ls.Start(t)
+	defer stop()
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "initialize",
+		ReqParams: fmt.Sprintf(`{
+	    "capabilities": {},
+	    "rootUri": %q,
+	    "processId": 12345
+	}`, tmpDir.URI)})
+	waitForWalkerPath(t, ss, wc, tmpDir)
+	ls.Notify(t, &langserver.CallRequest{
+		Method:    "initialized",
+		ReqParams: "{}",
+	})
+
+	// Verify main.tf was parsed
+	mod, err := ss.Modules.ModuleByPath(tmpDir.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedFiles := mod.ParsedModuleFiles.AsMap()
+	parsedFile, ok := parsedFiles["main.tf"]
+	if !ok {
+		t.Fatalf("file not parsed: %q", "main.tf")
+	}
+	if diff := cmp.Diff(originalSrc, string(parsedFile.Bytes)); diff != "" {
+		t.Fatalf("bytes mismatch for %q: %s", "main.tf", diff)
+	}
+
+	// Delete main.tf from disk
+	err = os.Remove(filepath.Join(tmpDir.Path(), "main.tf"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Verify main.tf still remains parsed
+	mod, err = ss.Modules.ModuleByPath(tmpDir.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedFiles = mod.ParsedModuleFiles.AsMap()
+	parsedFile, ok = parsedFiles["main.tf"]
+	if !ok {
+		t.Fatalf("file not parsed: %q", "main.tf")
+	}
+	if diff := cmp.Diff(originalSrc, string(parsedFile.Bytes)); diff != "" {
+		t.Fatalf("bytes mismatch for %q: %s", "main.tf", diff)
+	}
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "workspace/didChangeWatchedFiles",
+		ReqParams: fmt.Sprintf(`{
+    "changes": [
+        {
+            "uri": "%s/main.tf",
+            "type": 3
+        }
+    ]
+}`, TempDir(t).URI)})
+
+	// Verify main.tf was deleted
+	mod, err = ss.Modules.ModuleByPath(tmpDir.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedFiles = mod.ParsedModuleFiles.AsMap()
+	parsedFile, ok = parsedFiles["main.tf"]
+	if ok {
+		t.Fatalf("not expected file to be parsed: %q", "main.tf")
+	}
+}
+
+func TestLangServer_DidChangeWatchedFiles_change_dir(t *testing.T) {
 	tmpDir := TempDir(t)
 
 	InitPluginCache(t, tmpDir.Path())
@@ -183,7 +433,7 @@ func TestLangServer_DidChangeWatchedFiles_dir(t *testing.T) {
 	}
 
 	// Change main.tf on disk
-	newSrc := `variable "original" {
+	newSrc := `variable "new" {
   default = "foo"
 }
 `
@@ -229,5 +479,211 @@ func TestLangServer_DidChangeWatchedFiles_dir(t *testing.T) {
 	}
 	if diff := cmp.Diff(newSrc, string(parsedFile.Bytes)); diff != "" {
 		t.Fatalf("bytes mismatch for %q: %s", "main.tf", diff)
+	}
+}
+
+func TestLangServer_DidChangeWatchedFiles_create_dir(t *testing.T) {
+	tmpDir := TempDir(t)
+
+	InitPluginCache(t, tmpDir.Path())
+
+	originalSrc := `variable "original" {
+  default = "foo"
+}
+`
+	err := os.WriteFile(filepath.Join(tmpDir.Path(), "main.tf"), []byte(originalSrc), 0o755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ss, err := state.NewStateStore()
+	if err != nil {
+		t.Fatal(err)
+	}
+	wc := module.NewWalkerCollector()
+
+	ls := langserver.NewLangServerMock(t, NewMockSession(&MockSessionInput{
+		TerraformCalls: &exec.TerraformMockCalls{
+			PerWorkDir: map[string][]*mock.Call{
+				tmpDir.Path(): validTfMockCalls(),
+			},
+		},
+		StateStore:      ss,
+		WalkerCollector: wc,
+	}))
+	stop := ls.Start(t)
+	defer stop()
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "initialize",
+		ReqParams: fmt.Sprintf(`{
+	    "capabilities": {},
+	    "rootUri": %q,
+	    "processId": 12345
+	}`, tmpDir.URI)})
+	waitForWalkerPath(t, ss, wc, tmpDir)
+	ls.Notify(t, &langserver.CallRequest{
+		Method:    "initialized",
+		ReqParams: "{}",
+	})
+
+	// Verify main.tf was parsed
+	mod, err := ss.Modules.ModuleByPath(tmpDir.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedFiles := mod.ParsedModuleFiles.AsMap()
+	parsedFile, ok := parsedFiles["main.tf"]
+	if !ok {
+		t.Fatalf("file not parsed: %q", "main.tf")
+	}
+	if diff := cmp.Diff(originalSrc, string(parsedFile.Bytes)); diff != "" {
+		t.Fatalf("bytes mismatch for %q: %s", "main.tf", diff)
+	}
+
+	// Create new ./submodule w/ main.tf on disk
+	submodPath := filepath.Join(tmpDir.Path(), "submodule")
+	submodHandle := document.DirHandleFromPath(submodPath)
+	err = os.Mkdir(submodPath, 0o755)
+	if err != nil {
+		t.Fatal(err)
+	}
+	newSrc := `variable "new" {
+  default = "foo"
+}
+`
+	err = os.WriteFile(filepath.Join(submodPath, "main.tf"), []byte(newSrc), 0o755)
+	if err != nil {
+		t.Fatal(err)
+	}
+	InitPluginCache(t, submodHandle.Path())
+
+	// Verify submodule was not parsed yet
+	mod, err = ss.Modules.ModuleByPath(submodPath)
+	if err == nil {
+		t.Fatalf("%q: expected module not to be found", submodPath)
+	}
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "workspace/didChangeWatchedFiles",
+		ReqParams: fmt.Sprintf(`{
+    "changes": [
+        {
+            "uri": %q,
+            "type": 1
+        }
+    ]
+}`, submodHandle.URI)})
+	waitForWalkerPath(t, ss, wc, submodHandle)
+
+	// Verify submodule was parsed
+	mod, err = ss.Modules.ModuleByPath(submodPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedFiles = mod.ParsedModuleFiles.AsMap()
+	parsedFile, ok = parsedFiles["main.tf"]
+	if !ok {
+		t.Fatalf("file not parsed: %q", "main.tf")
+	}
+	if diff := cmp.Diff(newSrc, string(parsedFile.Bytes)); diff != "" {
+		t.Fatalf("bytes mismatch for %q: %s", "main.tf", diff)
+	}
+}
+
+func TestLangServer_DidChangeWatchedFiles_delete_dir(t *testing.T) {
+	tmpDir := TempDir(t)
+
+	InitPluginCache(t, tmpDir.Path())
+
+	originalSrc := `variable "original" {
+  default = "foo"
+}
+`
+	err := os.WriteFile(filepath.Join(tmpDir.Path(), "main.tf"), []byte(originalSrc), 0o755)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ss, err := state.NewStateStore()
+	if err != nil {
+		t.Fatal(err)
+	}
+	wc := module.NewWalkerCollector()
+
+	ls := langserver.NewLangServerMock(t, NewMockSession(&MockSessionInput{
+		TerraformCalls: &exec.TerraformMockCalls{
+			PerWorkDir: map[string][]*mock.Call{
+				tmpDir.Path(): validTfMockCalls(),
+			},
+		},
+		StateStore:      ss,
+		WalkerCollector: wc,
+	}))
+	stop := ls.Start(t)
+	defer stop()
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "initialize",
+		ReqParams: fmt.Sprintf(`{
+	    "capabilities": {},
+	    "rootUri": %q,
+	    "processId": 12345
+	}`, tmpDir.URI)})
+	waitForWalkerPath(t, ss, wc, tmpDir)
+	ls.Notify(t, &langserver.CallRequest{
+		Method:    "initialized",
+		ReqParams: "{}",
+	})
+
+	// Verify main.tf was parsed
+	mod, err := ss.Modules.ModuleByPath(tmpDir.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedFiles := mod.ParsedModuleFiles.AsMap()
+	parsedFile, ok := parsedFiles["main.tf"]
+	if !ok {
+		t.Fatalf("file not parsed: %q", "main.tf")
+	}
+	if diff := cmp.Diff(originalSrc, string(parsedFile.Bytes)); diff != "" {
+		t.Fatalf("bytes mismatch for %q: %s", "main.tf", diff)
+	}
+
+	// Delete directory from disk
+	err = os.RemoveAll(tmpDir.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Verify nothing has changed yet
+	mod, err = ss.Modules.ModuleByPath(tmpDir.Path())
+	if err != nil {
+		t.Fatal(err)
+	}
+	parsedFiles = mod.ParsedModuleFiles.AsMap()
+	parsedFile, ok = parsedFiles["main.tf"]
+	if !ok {
+		t.Fatalf("file not parsed: %q", "main.tf")
+	}
+	if diff := cmp.Diff(originalSrc, string(parsedFile.Bytes)); diff != "" {
+		t.Fatalf("bytes mismatch for %q: %s", "main.tf", diff)
+	}
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "workspace/didChangeWatchedFiles",
+		ReqParams: fmt.Sprintf(`{
+    "changes": [
+        {
+            "uri": %q,
+            "type": 3
+        }
+    ]
+}`, TempDir(t).URI)})
+
+	// Verify module is gone
+	_, err = ss.Modules.ModuleByPath(tmpDir.Path())
+	if err == nil {
+		t.Fatalf("expected module at %q to be gone", tmpDir.Path())
 	}
 }
