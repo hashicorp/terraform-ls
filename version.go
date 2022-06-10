b7a39bd07f6c3f9fb2c2b436819cfc91a9dd4201
Radek Simko <radek.simko@gmail.com>
2022-06-10 19:10:43 +0100

    main: allow version metadata to be set (#945)

--- version.go
+++ version.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	goversion "github.com/hashicorp/go-version"
 )
 
@@ -12,7 +10,10 @@ var version = "0.0.0"
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-var prerelease = "dev"
+var versionPrerelease = "dev"
+
+// Version (build) metadata, such as homebrew
+var versionMetadata = ""
 
 func init() {
 	// Verify that the version is proper semantic version, which should always be the case.
@@ -24,8 +25,14 @@ func init() {
 
 // VersionString returns the complete version string, including prerelease
 func VersionString() string {
-	if prerelease != "" {
-		return fmt.Sprintf("%s-%s", version, prerelease)
+	v := version
+	if versionPrerelease != "" {
+		v += "-" + versionPrerelease
 	}
-	return version
+
+	if versionMetadata != "" {
+		v += "+" + versionMetadata
+	}
+
+	return v
 }
