4c1ddc023d77a75a42aea72f94bfe290fbb4f0c5
Radek Simko <radek.simko@gmail.com>
2022-06-27 20:07:56 +0100

    fix: avoid crash on invalid URIs (#969)

--- internal/langserver/handlers/did_open.go
+++ internal/langserver/handlers/did_open.go
@@ -2,19 +2,35 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/creachadair/jrpc2"
 	"github.com/hashicorp/terraform-ls/internal/document"
 	"github.com/hashicorp/terraform-ls/internal/job"
-	ilsp "github.com/hashicorp/terraform-ls/internal/lsp"
 	lsp "github.com/hashicorp/terraform-ls/internal/protocol"
 	"github.com/hashicorp/terraform-ls/internal/state"
 	"github.com/hashicorp/terraform-ls/internal/terraform/exec"
 	"github.com/hashicorp/terraform-ls/internal/terraform/module"
 	op "github.com/hashicorp/terraform-ls/internal/terraform/module/operation"
+	"github.com/hashicorp/terraform-ls/internal/uri"
 )
 
 func (svc *service) TextDocumentDidOpen(ctx context.Context, params lsp.DidOpenTextDocumentParams) error {
-	dh := ilsp.HandleFromDocumentURI(params.TextDocument.URI)
+	docURI := string(params.TextDocument.URI)
+
+	// URIs are always checked during initialize request, but
+	// we still allow single-file mode, therefore invalid URIs
+	// can still land here, so we check for those.
+	if !uri.IsURIValid(docURI) {
+		jrpc2.ServerFromContext(ctx).Notify(ctx, "window/showMessage", &lsp.ShowMessageParams{
+			Type: lsp.Warning,
+			Message: fmt.Sprintf("Ignoring workspace folder (unsupport or invalid URI) %s."+
+				" This is most likely bug, please report it.", docURI),
+		})
+		return fmt.Errorf("invalid URI: %s", docURI)
+	}
+
+	dh := document.HandleFromURI(docURI)
 
 	err := svc.stateStore.DocumentStore.OpenDocument(dh, params.TextDocument.LanguageID,
 		int(params.TextDocument.Version), []byte(params.TextDocument.Text))
