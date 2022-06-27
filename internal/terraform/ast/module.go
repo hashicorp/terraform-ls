bfa5d9c146031edcfa180ab753549e3f07ebadaa
Radek Simko <radek.simko@gmail.com>
2022-06-27 18:28:10 +0100

    fix: avoid ignoring hidden tfvars files (#968)

--- internal/terraform/ast/module.go
+++ internal/terraform/ast/module.go
@@ -17,9 +17,21 @@ func (mf ModFilename) IsJSON() bool {
 }
 
 func IsModuleFilename(name string) bool {
-	return (strings.HasSuffix(name, ".tf") ||
-		strings.HasSuffix(name, ".tf.json")) &&
-		!isIgnoredFile(name)
+	if isIgnoredFile(name) {
+		// See https://github.com/hashicorp/terraform/blob/d35bc05/internal/configs/parser_config_dir.go#L107
+		return false
+	}
+
+	return strings.HasSuffix(name, ".tf") ||
+		strings.HasSuffix(name, ".tf.json")
+}
+
+// isIgnoredFile returns true if the given filename (which must not have a
+// directory path ahead of it) should be ignored as e.g. an editor swap file.
+func isIgnoredFile(name string) bool {
+	return strings.HasPrefix(name, ".") || // Unix-like hidden files
+		strings.HasSuffix(name, "~") || // vim
+		strings.HasPrefix(name, "#") && strings.HasSuffix(name, "#") // emacs
 }
 
 type ModFiles map[ModFilename]*hcl.File
