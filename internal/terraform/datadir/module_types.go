a7f99923cd027dcec43fcca0d4cd5e40b22453f6
Daniel Banck <daniel@dbanck.de>
2022-07-05 11:26:49 +0200

    Infer module type from string instead of *ModuleRecord

--- internal/terraform/datadir/module_types.go
+++ internal/terraform/datadir/module_types.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	tfaddr "github.com/hashicorp/terraform-registry-address"
+	tfmod "github.com/hashicorp/terraform-schema/module"
 )
 
 type ModuleType string
@@ -16,36 +17,29 @@ const (
 	GIT        ModuleType = "git"
 )
 
-var moduleSourceLocalPrefixes = []string{
-	"./",
-	"../",
-	".\\",
-	"..\\",
-}
-
 // GetModuleType parses source addresses to determine what kind of source the Terraform module comes
 // from. It currently supports detecting Terraform Registry modules, GitHub modules, Git modules, and
 // local file paths
-func (r *ModuleRecord) GetModuleType() ModuleType {
+func GetModuleType(sourceAddr string) ModuleType {
 	// Example: terraform-aws-modules/ec2-instance/aws
 	// Example: registry.terraform.io/terraform-aws-modules/vpc/aws
-	moduleSourceRegistry, err := tfaddr.ParseModuleSource(r.SourceAddr)
+	moduleSourceRegistry, err := tfaddr.ParseModuleSource(sourceAddr)
 	if err == nil && moduleSourceRegistry.Package.Host == "registry.terraform.io" {
 		return TFREGISTRY
 	}
 
 	// Example: github.com/terraform-aws-modules/terraform-aws-security-group
-	if strings.HasPrefix(r.SourceAddr, "github.com/") {
+	if strings.HasPrefix(sourceAddr, "github.com/") {
 		return GITHUB
 	}
 
 	// Example: git::https://example.com/vpc.git
-	if strings.HasPrefix(r.SourceAddr, "git::") {
+	if strings.HasPrefix(sourceAddr, "git::") {
 		return GIT
 	}
 
 	// Local file paths
-	if isModuleSourceLocal(r.SourceAddr) {
+	if isModuleSourceLocal(sourceAddr) {
 		return LOCAL
 	}
 
@@ -53,7 +47,7 @@ func (r *ModuleRecord) GetModuleType() ModuleType {
 }
 
 func isModuleSourceLocal(raw string) bool {
-	for _, prefix := range moduleSourceLocalPrefixes {
+	for _, prefix := range tfmod.ModuleSourceLocalPrefixes {
 		if strings.HasPrefix(raw, prefix) {
 			return true
 		}
