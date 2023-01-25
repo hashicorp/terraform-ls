1d788e9a0c98ff3bf839aa47a0b64d2f152b152b
Daniel Banck <daniel@dbanck.de>
2023-01-25 14:47:47 +0100

    Obtain version from version/
    This complies with the requirement of needing a version/VERSION file

--- version.go
+++ version.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	_ "embed"
+	"strings"
+
 	goversion "github.com/hashicorp/go-version"
 )
 
-// The main version number that is being run at the moment.
-var version = "0.0.0"
+var (
+	// The next version number that will be released. This will be updated after every release
+	// Version must conform to the format expected by github.com/hashicorp/go-version
+	// for tests to work.
+	// A pre-release marker for the version can also be specified (e.g -dev). If this is omitted
+	// then it means that it is a final release. Otherwise, this is a pre-release
+	// such as "dev" (in development), "beta", "rc1", etc.
+	//go:embed version/VERSION
+	fullVersion string
 
-// A pre-release marker for the version. If this is "" (empty string)
-// then it means that it is a final release. Otherwise, this is a pre-release
-// such as "dev" (in development), "beta", "rc1", etc.
-var versionPrerelease = "dev"
+	version, versionPrerelease, _ = strings.Cut(fullVersion, "-")
 
-// Version (build) metadata, such as homebrew
-var versionMetadata = ""
+	// https://semver.org/#spec-item-10
+	versionMetadata = ""
+)
 
 func init() {
 	// Verify that the version is proper semantic version, which should always be the case.
