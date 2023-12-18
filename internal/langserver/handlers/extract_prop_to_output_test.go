19791adf9a04519fb412db21c435b7047300bdd4
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2023-12-18 18:08:42 +0800

    support refactor.extract.propToOutput

---
+++ internal/langserver/handlers/extract_prop_to_output_test.go
@@ -0,0 +1,483 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package handlers
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/creachadair/jrpc2"
+	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/terraform-ls/internal/langserver"
+	"github.com/hashicorp/terraform-ls/internal/langserver/session"
+	"github.com/hashicorp/terraform-ls/internal/state"
+	"github.com/hashicorp/terraform-ls/internal/terraform/exec"
+	"github.com/hashicorp/terraform-ls/internal/walker"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestLangServer_extractPropToOutput_withoutInitialization(t *testing.T) {
+	ls := langserver.NewLangServerMock(t, NewMockSession(nil))
+	stop := ls.Start(t)
+	defer stop()
+
+	ls.CallAndExpectError(t, &langserver.CallRequest{
+		Method: "textDocument/formatting",
+		ReqParams: fmt.Sprintf(`{
+		"textDocument": {
+			"version": 0,
+			"languageId": "terraform",
+			"text": "provider \"github\" {}",
+			"uri": "%s/main.tf"
+		}
+	}`, TempDir(t).URI),
+	}, session.SessionNotInitialized.Err())
+}
+
+func TestLangServer_ExtractPropToOutput_basic(t *testing.T) {
+	tmpDir := TempDir(t)
+
+	ss, err := state.NewStateStore()
+	if err != nil {
+		t.Fatal(err)
+	}
+	wc := walker.NewWalkerCollector()
+
+	ls := langserver.NewLangServerMock(t, NewMockSession(&MockSessionInput{
+		StateStore:      ss,
+		WalkerCollector: wc,
+		TerraformCalls: &exec.TerraformMockCalls{
+			PerWorkDir: map[string][]*mock.Call{
+				tmpDir.Path(): {
+					{
+						Method:        "Version",
+						Repeatability: 1,
+						Arguments: []interface{}{
+							mock.AnythingOfType(""),
+						},
+						ReturnArguments: []interface{}{
+							version.Must(version.NewVersion("0.12.0")),
+							nil,
+							nil,
+						},
+					},
+					{
+						Method:        "GetExecPath",
+						Repeatability: 1,
+						ReturnArguments: []interface{}{
+							"",
+						},
+					},
+					{
+						Method:        "Format",
+						Repeatability: 1,
+						Arguments: []interface{}{
+							mock.AnythingOfType(""),
+							[]byte("provider  \"test\"   {\n\n}\n"),
+						},
+						ReturnArguments: []interface{}{
+							[]byte("provider \"test\" {\n\n}\n"),
+							nil,
+						},
+					},
+				},
+			},
+		},
+	}))
+	stop := ls.Start(t)
+	defer stop()
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "initialize",
+		ReqParams: fmt.Sprintf(`{
+	    "capabilities": {},
+	    "rootUri": %q,
+	    "processId": 12345
+	}`, tmpDir.URI),
+	})
+	waitForWalkerPath(t, ss, wc, tmpDir)
+	ls.Notify(t, &langserver.CallRequest{
+		Method:    "initialized",
+		ReqParams: "{}",
+	})
+	ls.Call(t, &langserver.CallRequest{
+		Method: "textDocument/didOpen",
+		ReqParams: fmt.Sprintf(`{
+		"textDocument": {
+			"version": 0,
+			"languageId": "terraform",
+			"text": "provider \"test\" {}\n\nresource \"test_resource\" \"test\" {\n    name = \"testname\"\n}",
+			"uri": "%s/main.tf"
+		}
+	}`, tmpDir.URI),
+	})
+	waitForAllJobs(t, ss)
+
+	ls.CallAndExpectResponse(t, &langserver.CallRequest{
+		Method: "textDocument/codeAction",
+		ReqParams: fmt.Sprintf(`{
+  "textDocument": {
+    "uri": "%s/main.tf"
+  },
+  "range": {
+    "start": {
+      "line": 3,
+      "character": 17 
+    },
+    "end": {
+      "line": 3,
+      "character": 17
+    }
+  },
+  "context": {
+    "only": [
+      "refactor.extract.propToOut"
+    ]
+  }
+}`, tmpDir.URI),
+	}, `{
+			"jsonrpc": "2.0",
+			"id": 3,
+			"result": [
+				{
+					"range": {
+						"start": { "line": 0, "character": 0 },
+						"end": { "line": 1, "character": 0 }
+					},
+					"newText": "provider \"test\" {\n"
+				}
+			]
+		}`)
+}
+
+func TestLangServer_ExtractPropToOutput_oldVersion(t *testing.T) {
+	tmpDir := TempDir(t)
+
+	ss, err := state.NewStateStore()
+	if err != nil {
+		t.Fatal(err)
+	}
+	wc := walker.NewWalkerCollector()
+
+	ls := langserver.NewLangServerMock(t, NewMockSession(&MockSessionInput{
+		StateStore:      ss,
+		WalkerCollector: wc,
+		TerraformCalls: &exec.TerraformMockCalls{
+			PerWorkDir: map[string][]*mock.Call{
+				tmpDir.Path(): {
+					{
+						Method:        "Version",
+						Repeatability: 1,
+						Arguments: []interface{}{
+							mock.AnythingOfType(""),
+						},
+						ReturnArguments: []interface{}{
+							version.Must(version.NewVersion("0.7.6")),
+							nil,
+							nil,
+						},
+					},
+					{
+						Method:        "GetExecPath",
+						Repeatability: 1,
+						ReturnArguments: []interface{}{
+							"",
+						},
+					},
+					{
+						Method:        "Format",
+						Repeatability: 1,
+						Arguments: []interface{}{
+							mock.AnythingOfType(""),
+							[]byte("provider  \"test\"   {\n\n}\n"),
+						},
+						ReturnArguments: []interface{}{
+							nil,
+							errors.New("not implemented"),
+						},
+					},
+				},
+			},
+		},
+	}))
+	stop := ls.Start(t)
+	defer stop()
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "initialize",
+		ReqParams: fmt.Sprintf(`{
+	    "capabilities": {},
+	    "rootUri": %q,
+	    "processId": 12345
+	}`, tmpDir.URI),
+	})
+	waitForWalkerPath(t, ss, wc, tmpDir)
+
+	ls.Notify(t, &langserver.CallRequest{
+		Method:    "initialized",
+		ReqParams: "{}",
+	})
+	ls.Call(t, &langserver.CallRequest{
+		Method: "textDocument/didOpen",
+		ReqParams: fmt.Sprintf(`{
+		"textDocument": {
+			"version": 0,
+			"languageId": "terraform",
+			"text": "provider  \"test\"   {\n\n}\n",
+			"uri": "%s/main.tf"
+		}
+	}`, tmpDir.URI),
+	})
+	waitForAllJobs(t, ss)
+
+	ls.CallAndExpectError(t, &langserver.CallRequest{
+		Method: "textDocument/formatting",
+		ReqParams: fmt.Sprintf(`{
+			"textDocument": {
+				"uri": "%s/main.tf"
+			}
+		}`, tmpDir.URI),
+	}, jrpc2.SystemError.Err())
+}
+
+func TestLangServer_extractPropToOutput_variables(t *testing.T) {
+	tmpDir := TempDir(t)
+
+	ss, err := state.NewStateStore()
+	if err != nil {
+		t.Fatal(err)
+	}
+	wc := walker.NewWalkerCollector()
+
+	ls := langserver.NewLangServerMock(t, NewMockSession(&MockSessionInput{
+		StateStore:      ss,
+		WalkerCollector: wc,
+		TerraformCalls: &exec.TerraformMockCalls{
+			PerWorkDir: map[string][]*mock.Call{
+				tmpDir.Path(): {
+					{
+						Method:        "Version",
+						Repeatability: 1,
+						Arguments: []interface{}{
+							mock.AnythingOfType(""),
+						},
+						ReturnArguments: []interface{}{
+							version.Must(version.NewVersion("0.12.0")),
+							nil,
+							nil,
+						},
+					},
+					{
+						Method:        "GetExecPath",
+						Repeatability: 1,
+						ReturnArguments: []interface{}{
+							"",
+						},
+					},
+					{
+						Method:        "Format",
+						Repeatability: 1,
+						Arguments: []interface{}{
+							mock.AnythingOfType(""),
+							[]byte("test  = \"dev\""),
+						},
+						ReturnArguments: []interface{}{
+							[]byte("test = \"dev\""),
+							nil,
+						},
+					},
+				},
+			},
+		},
+	}))
+	stop := ls.Start(t)
+	defer stop()
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "initialize",
+		ReqParams: fmt.Sprintf(`{
+	    "capabilities": {},
+	    "rootUri": %q,
+	    "processId": 12345
+	}`, tmpDir.URI),
+	})
+	waitForWalkerPath(t, ss, wc, tmpDir)
+
+	ls.Notify(t, &langserver.CallRequest{
+		Method:    "initialized",
+		ReqParams: "{}",
+	})
+	ls.Call(t, &langserver.CallRequest{
+		Method: "textDocument/didOpen",
+		ReqParams: fmt.Sprintf(`{
+		"textDocument": {
+			"version": 0,
+			"languageId": "terraform-vars",
+			"text": "test  = \"dev\"",
+			"uri": "%s/terraform.tfvars"
+		}
+	}`, tmpDir.URI),
+	})
+	waitForAllJobs(t, ss)
+
+	ls.CallAndExpectResponse(t, &langserver.CallRequest{
+		Method: "textDocument/formatting",
+		ReqParams: fmt.Sprintf(`{
+			"textDocument": {
+				"uri": "%s/terraform.tfvars"
+			}
+		}`, tmpDir.URI),
+	}, `{
+			"jsonrpc": "2.0",
+			"id": 3,
+			"result": [
+				{
+					"range": {
+						"start": { "line": 0, "character": 0 },
+						"end": { "line": 0, "character": 13 }
+					},
+					"newText": "test = \"dev\""
+				}
+			]
+		}`)
+}
+
+func TestLangServer_extractPropToOutput_diffBug(t *testing.T) {
+	tmpDir := TempDir(t)
+
+	cfg := `resource "aws_lambda_function" "f" {
+  environment {
+    variables = {
+      a = "b"
+    }
+  }
+}
+`
+	formattedCfg := `resource "aws_lambda_function" "f" {
+  environment {
+    variables = {
+      a = "b"
+    }
+  }
+}
+`
+
+	ss, err := state.NewStateStore()
+	if err != nil {
+		t.Fatal(err)
+	}
+	wc := walker.NewWalkerCollector()
+
+	ls := langserver.NewLangServerMock(t, NewMockSession(&MockSessionInput{
+		StateStore:      ss,
+		WalkerCollector: wc,
+		TerraformCalls: &exec.TerraformMockCalls{
+			PerWorkDir: map[string][]*mock.Call{
+				tmpDir.Path(): {
+					{
+						Method:        "Version",
+						Repeatability: 1,
+						Arguments: []interface{}{
+							mock.AnythingOfType(""),
+						},
+						ReturnArguments: []interface{}{
+							version.Must(version.NewVersion("0.12.0")),
+							nil,
+							nil,
+						},
+					},
+					{
+						Method:        "GetExecPath",
+						Repeatability: 1,
+						ReturnArguments: []interface{}{
+							"",
+						},
+					},
+					{
+						Method:        "Format",
+						Repeatability: 1,
+						Arguments: []interface{}{
+							mock.AnythingOfType("*context.valueCtx"),
+							[]byte(cfg),
+						},
+						ReturnArguments: []interface{}{
+							[]byte(formattedCfg),
+							nil,
+						},
+					},
+				},
+			},
+		},
+	}))
+	stop := ls.Start(t)
+	defer stop()
+
+	ls.Call(t, &langserver.CallRequest{
+		Method: "initialize",
+		ReqParams: fmt.Sprintf(`{
+	    "capabilities": {},
+	    "rootUri": %q,
+	    "processId": 12345
+	}`, tmpDir.URI),
+	})
+	waitForWalkerPath(t, ss, wc, tmpDir)
+
+	ls.Notify(t, &langserver.CallRequest{
+		Method:    "initialized",
+		ReqParams: "{}",
+	})
+	ls.Call(t, &langserver.CallRequest{
+		Method: "textDocument/didOpen",
+		ReqParams: fmt.Sprintf(`{
+		"textDocument": {
+			"version": 0,
+			"languageId": "terraform",
+			"text": `+fmt.Sprintf("%q", cfg)+`,
+			"uri": "%s/main.tf"
+		}
+	}`, tmpDir.URI),
+	})
+	waitForAllJobs(t, ss)
+
+	ls.CallAndExpectResponse(t, &langserver.CallRequest{
+		Method: "textDocument/formatting",
+		ReqParams: fmt.Sprintf(`{
+			"textDocument": {
+				"uri": "%s/main.tf"
+			}
+		}`, tmpDir.URI),
+	}, `{
+			"jsonrpc": "2.0",
+			"id": 3,
+			"result": [
+				{
+					"range": {
+						"start": {
+							"line": 1,
+							"character": 0
+						},
+						"end": {
+							"line": 5,
+							"character": 0
+						}
+					},
+					"newText": "  environment {\n    variables = {\n      a = \"b\"\n"
+				},
+				{
+					"range": {
+						"start": {
+							"line": 6,
+							"character": 0
+						},
+						"end": {
+							"line": 6,
+							"character": 0
+						}
+					},
+					"newText":"  }\n"
+				}
+			]
+		}`)
+}
--- internal/lsp/code_actions.go
+++ internal/lsp/code_actions.go
@@ -5,6 +5,7 @@ package lsp
 
 import (
 	"sort"
+	"strings"
 
 	lsp "github.com/hashicorp/terraform-ls/internal/protocol"
 )
@@ -12,31 +13,31 @@ import (
 const (
 	// SourceFormatAllTerraform is a Terraform specific format code action.
 	SourceFormatAllTerraform = "source.formatAll.terraform"
+	ExtractPropertyToOutput  = "refactor.extract.propToOut"
 )
 
 type CodeActions map[lsp.CodeActionKind]bool
 
-var (
-	// `source.*`: Source code actions apply to the entire file. They must be explicitly
-	// requested and will not show in the normal lightbulb menu. Source actions
-	// can be run on save using editor.codeActionsOnSave and are also shown in
-	// the source context menu.
-	// For action definitions, refer to: https://code.visualstudio.com/api/references/vscode-api#CodeActionKind
+// `source.*`: Source code actions apply to the entire file. They must be explicitly
+// requested and will not show in the normal lightbulb menu. Source actions
+// can be run on save using editor.codeActionsOnSave and are also shown in
+// the source context menu.
+// For action definitions, refer to: https://code.visualstudio.com/api/references/vscode-api#CodeActionKind
 
-	// `source.fixAll`: Fix all actions automatically fix errors that have a clear fix that do
-	// not require user input. They should not suppress errors or perform unsafe
-	// fixes such as generating new types or classes.
-	// ** We don't support this as terraform fmt only adjusts style**
-	// lsp.SourceFixAll: true,
+// `source.fixAll`: Fix all actions automatically fix errors that have a clear fix that do
+// not require user input. They should not suppress errors or perform unsafe
+// fixes such as generating new types or classes.
+// ** We don't support this as terraform fmt only adjusts style**
+// lsp.SourceFixAll: true,
 
-	// `source.formatAll`: Generic format code action.
-	// We do not register this for terraform to allow fine grained selection of actions.
-	// A user should be able to set `source.formatAll` to true, and source.formatAll.terraform to false to allow all
-	// files to be formatted, but not terraform files (or vice versa).
-	SupportedCodeActions = CodeActions{
-		SourceFormatAllTerraform: true,
-	}
-)
+// `source.formatAll`: Generic format code action.
+// We do not register this for terraform to allow fine grained selection of actions.
+// A user should be able to set `source.formatAll` to true, and source.formatAll.terraform to false to allow all
+// files to be formatted, but not terraform files (or vice versa).
+var SupportedCodeActions = CodeActions{
+	SourceFormatAllTerraform: true,
+	ExtractPropertyToOutput:  true,
+}
 
 func (c CodeActions) AsSlice() []lsp.CodeActionKind {
 	s := make([]lsp.CodeActionKind, 0)
@@ -54,8 +55,10 @@ func (ca CodeActions) Only(only []lsp.CodeActionKind) CodeActions {
 	wanted := make(CodeActions, 0)
 
 	for _, kind := range only {
-		if v, ok := ca[kind]; ok {
-			wanted[kind] = v
+		for n, v := range ca {
+			if strings.HasPrefix(string(n), string(kind)) {
+				wanted[n] = v
+			}
 		}
 	}
 
