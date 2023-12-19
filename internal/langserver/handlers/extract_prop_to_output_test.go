f6a07e6dfa9ba3367db11e45b34ed372e315bb9a
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2023-12-19 14:11:38 +0800

    update testcase

--- internal/langserver/handlers/extract_prop_to_output_test.go
+++ internal/langserver/handlers/extract_prop_to_output_test.go
@@ -24,15 +24,27 @@ func TestLangServer_extractPropToOutput_withoutInitialization(t *testing.T) {
 	defer stop()
 
 	ls.CallAndExpectError(t, &langserver.CallRequest{
-		Method: "textDocument/formatting",
+		Method: "textDocument/codeAction",
 		ReqParams: fmt.Sprintf(`{
-		"textDocument": {
-			"version": 0,
-			"languageId": "terraform",
-			"text": "provider \"github\" {}",
-			"uri": "%s/main.tf"
-		}
-	}`, TempDir(t).URI),
+  "textDocument": {
+    "uri": "%s/main.tf"
+  },
+  "range": {
+    "start": {
+      "line": 4,
+      "character": 17 
+    },
+    "end": {
+      "line": 4,
+      "character": 17
+    }
+  },
+  "context": {
+    "only": [
+      "refactor.extract.propToOut"
+    ]
+  }
+}`, TempDir(t).URI),
 	}, session.SessionNotInitialized.Err())
 }
 
@@ -70,18 +82,6 @@ func TestLangServer_ExtractPropToOutput_basic(t *testing.T) {
 							"",
 						},
 					},
-					{
-						Method:        "Format",
-						Repeatability: 1,
-						Arguments: []interface{}{
-							mock.AnythingOfType(""),
-							[]byte("provider  \"test\"   {\n\n}\n"),
-						},
-						ReturnArguments: []interface{}{
-							[]byte("provider \"test\" {\n\n}\n"),
-							nil,
-						},
-					},
 				},
 			},
 		},
@@ -108,7 +108,7 @@ func TestLangServer_ExtractPropToOutput_basic(t *testing.T) {
 		"textDocument": {
 			"version": 0,
 			"languageId": "terraform",
-			"text": "provider \"test\" {}\n\nresource \"test_resource\" \"test\" {\n    name = \"testname\"\n}",
+			"text": "provider \"test\"{}\n\nresource \"test_resource\" \"test\"{\n    name = \"test\"\n}",
 			"uri": "%s/main.tf"
 		}
 	}`, tmpDir.URI),
@@ -124,11 +124,11 @@ func TestLangServer_ExtractPropToOutput_basic(t *testing.T) {
   "range": {
     "start": {
       "line": 3,
-      "character": 17 
+      "character": 14 
     },
     "end": {
       "line": 3,
-      "character": 17
+      "character": 14
     }
   },
   "context": {
@@ -137,19 +137,35 @@ func TestLangServer_ExtractPropToOutput_basic(t *testing.T) {
     ]
   }
 }`, tmpDir.URI),
-	}, `{
-			"jsonrpc": "2.0",
-			"id": 3,
-			"result": [
-				{
-					"range": {
-						"start": { "line": 0, "character": 0 },
-						"end": { "line": 1, "character": 0 }
-					},
-					"newText": "provider \"test\" {\n"
-				}
-			]
-		}`)
+	}, fmt.Sprintf(`{
+  "jsonrpc": "2.0",
+  "id": 3,
+  "result": [
+    {
+      "title": "Extract Property to Output",
+      "kind": "refactor.extract.propToOut",
+      "edit": {
+        "changes": {
+          "%s/main.tf": [
+            {
+              "range": {
+                "start": {
+                  "line": 6,
+                  "character": 6
+                },
+                "end": {
+                  "line": 6,
+                  "character": 6
+                }
+              },
+              "newText": "\noutput \"test_resource_test_name\" {\n  value = test_resource.test.name\n}\n"
+            }
+          ]
+        }
+      }
+    }
+  ]
+}`, tmpDir.URI))
 }
 
 func TestLangServer_ExtractPropToOutput_oldVersion(t *testing.T) {
@@ -339,144 +355,6 @@ func TestLangServer_extractPropToOutput_variables(t *testing.T) {
 						"end": { "line": 0, "character": 13 }
 					},
 					"newText": "test = \"dev\""
-				}
-			]
-		}`)
-}
-
-func TestLangServer_extractPropToOutput_diffBug(t *testing.T) {
-	tmpDir := TempDir(t)
-
-	cfg := `resource "aws_lambda_function" "f" {
-  environment {
-    variables = {
-      a = "b"
-    }
-  }
-}
-`
-	formattedCfg := `resource "aws_lambda_function" "f" {
-  environment {
-    variables = {
-      a = "b"
-    }
-  }
-}
-`
-
-	ss, err := state.NewStateStore()
-	if err != nil {
-		t.Fatal(err)
-	}
-	wc := walker.NewWalkerCollector()
-
-	ls := langserver.NewLangServerMock(t, NewMockSession(&MockSessionInput{
-		StateStore:      ss,
-		WalkerCollector: wc,
-		TerraformCalls: &exec.TerraformMockCalls{
-			PerWorkDir: map[string][]*mock.Call{
-				tmpDir.Path(): {
-					{
-						Method:        "Version",
-						Repeatability: 1,
-						Arguments: []interface{}{
-							mock.AnythingOfType(""),
-						},
-						ReturnArguments: []interface{}{
-							version.Must(version.NewVersion("0.12.0")),
-							nil,
-							nil,
-						},
-					},
-					{
-						Method:        "GetExecPath",
-						Repeatability: 1,
-						ReturnArguments: []interface{}{
-							"",
-						},
-					},
-					{
-						Method:        "Format",
-						Repeatability: 1,
-						Arguments: []interface{}{
-							mock.AnythingOfType("*context.valueCtx"),
-							[]byte(cfg),
-						},
-						ReturnArguments: []interface{}{
-							[]byte(formattedCfg),
-							nil,
-						},
-					},
-				},
-			},
-		},
-	}))
-	stop := ls.Start(t)
-	defer stop()
-
-	ls.Call(t, &langserver.CallRequest{
-		Method: "initialize",
-		ReqParams: fmt.Sprintf(`{
-	    "capabilities": {},
-	    "rootUri": %q,
-	    "processId": 12345
-	}`, tmpDir.URI),
-	})
-	waitForWalkerPath(t, ss, wc, tmpDir)
-
-	ls.Notify(t, &langserver.CallRequest{
-		Method:    "initialized",
-		ReqParams: "{}",
-	})
-	ls.Call(t, &langserver.CallRequest{
-		Method: "textDocument/didOpen",
-		ReqParams: fmt.Sprintf(`{
-		"textDocument": {
-			"version": 0,
-			"languageId": "terraform",
-			"text": `+fmt.Sprintf("%q", cfg)+`,
-			"uri": "%s/main.tf"
-		}
-	}`, tmpDir.URI),
-	})
-	waitForAllJobs(t, ss)
-
-	ls.CallAndExpectResponse(t, &langserver.CallRequest{
-		Method: "textDocument/formatting",
-		ReqParams: fmt.Sprintf(`{
-			"textDocument": {
-				"uri": "%s/main.tf"
-			}
-		}`, tmpDir.URI),
-	}, `{
-			"jsonrpc": "2.0",
-			"id": 3,
-			"result": [
-				{
-					"range": {
-						"start": {
-							"line": 1,
-							"character": 0
-						},
-						"end": {
-							"line": 5,
-							"character": 0
-						}
-					},
-					"newText": "  environment {\n    variables = {\n      a = \"b\"\n"
-				},
-				{
-					"range": {
-						"start": {
-							"line": 6,
-							"character": 0
-						},
-						"end": {
-							"line": 6,
-							"character": 0
-						}
-					},
-					"newText":"  }\n"
 				}
 			]
 		}`)
