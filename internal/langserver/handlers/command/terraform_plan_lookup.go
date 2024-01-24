3afc4c26ef17e2d89bcbbea9c74ca2ee1874e465
James Pogran <jpogran@outlook.com>
2024-01-24 11:58:21 -0500

    Terminal Link Command handler
    This adds a new command handler for the terraform.plan.lookup command. This command is used to lookup the location of a resource in a plan file. This is used by the terminal provider to provide hyperlinks to the plan file.

---
+++ internal/langserver/handlers/command/terraform_plan_lookup.go
@@ -0,0 +1,81 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package command
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-ls/internal/langserver/cmd"
+	"github.com/hashicorp/terraform-ls/internal/uri"
+)
+
+const terraformPlanLookupRequestVersion = 0
+
+type vscodeRange struct {
+	StartLine      int `json:"startLine"`
+	StartCharacter int `json:"startCharacter"`
+	EndLine        int `json:"endLine"`
+	EndCharacter   int `json:"endCharacter"`
+}
+
+type terraformPlanLookupResponse struct {
+	FormatVersion int         `json:"v"`
+	Range         vscodeRange `json:"range"`
+	FileUri       string      `json:"fileUri"`
+}
+
+func (h *CmdHandler) TerraformPlanLookupHandler(ctx context.Context, args cmd.CommandArgs) (interface{}, error) {
+	response := terraformPlanLookupResponse{
+		FormatVersion: terraformPlanLookupRequestVersion,
+	}
+
+	// NOTE: Do not return errors for this handler as this is
+	// for hyperlinks in a terminal. We would spam the message box
+	// if a user hovered over things that were not valid
+
+	// Get the module dir, if it exists
+	modUri, ok := args.GetString("uri")
+	if !ok || modUri == "" {
+		return response, nil
+	}
+
+	// make sure this is a valid uri
+	if !uri.IsURIValid(modUri) {
+		return response, nil
+	}
+
+	// get the module path
+	modPath, err := uri.PathFromURI(modUri)
+	if err != nil {
+		return response, nil
+	}
+
+	// find the mod state in memdb
+	mod, _ := h.StateStore.Modules.ModuleByPath(modPath)
+	if mod == nil {
+		return response, nil
+	}
+
+	line, _ := args.GetString("line")
+
+	// iterate through the ref targets and find the one that matches
+	// the address given by the client
+	for _, target := range mod.RefTargets {
+		if target.Addr.String() == line {
+			response.FileUri = fmt.Sprintf("%v/%v", modUri, target.RangePtr.Filename)
+
+			// TODO: these lines are off because of the drift between
+			// what terraform plan shows versus what is in the actual file
+			response.Range.StartLine = target.RangePtr.Start.Line
+			response.Range.StartCharacter = target.RangePtr.Start.Column
+			response.Range.EndLine = target.RangePtr.End.Line
+			response.Range.EndCharacter = target.RangePtr.End.Column
+
+			return response, nil
+		}
+	}
+
+	return response, nil
+}
