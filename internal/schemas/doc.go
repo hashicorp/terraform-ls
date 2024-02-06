df4fb73944459edc6012021a8e637331a9cac7e7
Ansgar Mertens <ansgar@hashicorp.com>
2024-02-06 09:54:11 +0100

    chore: fix typo in comment

--- internal/schemas/doc.go
+++ internal/schemas/doc.go
@@ -1,7 +1,7 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-// Package schemas embeds provder schemas for completion support, in particular,
+// Package schemas embeds provider schemas for completion support, in particular,
 // for uninitialized modules
 package schemas
 
