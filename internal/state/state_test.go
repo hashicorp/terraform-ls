bede5ff3372df79ea704cb00c1892e791786a5c7
Daniel Banck <daniel@dbanck.de>
2024-06-06 21:07:37 +0200

    state: Remove module state
    The state has been moved to separate features. The global state now
    only contains entities that are relevant to everything.
    
    The new change tracking has been partially lifted into the features and
    only the generic part is kept in the global state.

--- internal/state/state_test.go
+++ internal/state/state_test.go
@@ -5,11 +5,6 @@ package state
 
 import "testing"
 
-var (
-	_ ModuleReader = &ModuleStore{}
-	_ SchemaReader = &ProviderSchemaStore{}
-)
-
 func TestDbSchema_Validate(t *testing.T) {
 	err := dbSchema.Validate()
 	if err != nil {
