3d3977e09289e0fd1d7f00d9d69fc8f2d0b6b606
Daniel Banck <daniel@dbanck.de>
2024-06-06 21:05:26 +0200

    decoder: Fetch matching decoders from features
    The global decoder now only acts as a "proxy" and will use a more
    specific decoder from a feature depending on the language ID.

--- internal/decoder/path_reader.go
+++ internal/decoder/path_reader.go
@@ -7,66 +7,34 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/hcl-lang/decoder"
 	"github.com/hashicorp/hcl-lang/lang"
-	ilsp "github.com/hashicorp/terraform-ls/internal/lsp"
-	"github.com/hashicorp/terraform-ls/internal/state"
-	tfaddr "github.com/hashicorp/terraform-registry-address"
-	tfmod "github.com/hashicorp/terraform-schema/module"
-	"github.com/hashicorp/terraform-schema/registry"
 )
 
-type ModuleReader interface {
-	ModuleByPath(modPath string) (*state.Module, error)
-	List() ([]*state.Module, error)
-	ModuleCalls(modPath string) (tfmod.ModuleCalls, error)
-	LocalModuleMeta(modPath string) (*tfmod.Meta, error)
-	RegistryModuleMeta(addr tfaddr.Module, cons version.Constraints) (*registry.ModuleData, error)
+type PathReaderMap map[string]decoder.PathReader
+
+// GlobalPathReader is a PathReader that delegates language specific PathReaders
+// that usually come from features.
+type GlobalPathReader struct {
+	PathReaderMap PathReaderMap
 }
 
-type PathReader struct {
-	ModuleReader ModuleReader
-	SchemaReader state.SchemaReader
-}
+var _ decoder.PathReader = &GlobalPathReader{}
 
-var _ decoder.PathReader = &PathReader{}
-
-func (mr *PathReader) Paths(ctx context.Context) []lang.Path {
+func (mr *GlobalPathReader) Paths(ctx context.Context) []lang.Path {
 	paths := make([]lang.Path, 0)
 
-	modList, err := mr.ModuleReader.List()
-	if err != nil {
-		return paths
-	}
-	for _, mod := range modList {
-		paths = append(paths, lang.Path{
-			Path:       mod.Path,
-			LanguageID: ilsp.Terraform.String(),
-		})
-		if len(mod.ParsedVarsFiles) > 0 {
-			paths = append(paths, lang.Path{
-				Path:       mod.Path,
-				LanguageID: ilsp.Tfvars.String(),
-			})
-		}
+	for _, feature := range mr.PathReaderMap {
+		paths = append(paths, feature.Paths(ctx)...)
 	}
 
 	return paths
 }
 
-func (mr *PathReader) PathContext(path lang.Path) (*decoder.PathContext, error) {
-	mod, err := mr.ModuleReader.ModuleByPath(path.Path)
-	if err != nil {
-		return nil, err
+func (mr *GlobalPathReader) PathContext(path lang.Path) (*decoder.PathContext, error) {
+	if feature, ok := mr.PathReaderMap[path.LanguageID]; ok {
+		return feature.PathContext(path)
 	}
 
-	switch path.LanguageID {
-	case ilsp.Terraform.String():
-		return modulePathContext(mod, mr.SchemaReader, mr.ModuleReader)
-	case ilsp.Tfvars.String():
-		return varsPathContext(mod)
-	}
-
-	return nil, fmt.Errorf("unknown language ID: %q", path.LanguageID)
+	return nil, fmt.Errorf("no feature found for language %s", path.LanguageID)
 }
