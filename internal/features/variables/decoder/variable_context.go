1e99e8397c71cba9ab5896e9f57bd644fd4f7dd6
Daniel Banck <daniel@dbanck.de>
2024-06-06 20:55:49 +0200

    features: Add feature for variable definition files
    This moves all logic related to Terraform's variable definition
    files into a feature.

---
+++ internal/features/variables/decoder/variable_context.go
@@ -0,0 +1,54 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package decoder
+
+import (
+	"github.com/hashicorp/hcl-lang/decoder"
+	"github.com/hashicorp/hcl-lang/reference"
+	"github.com/hashicorp/hcl-lang/schema"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/terraform-ls/internal/features/variables/ast"
+	"github.com/hashicorp/terraform-ls/internal/features/variables/state"
+	tfschema "github.com/hashicorp/terraform-schema/schema"
+)
+
+func variablePathContext(mod *state.VariableRecord, moduleReader ModuleReader, useAnySchema bool) (*decoder.PathContext, error) {
+	variables, _ := moduleReader.ModuleInputs(mod.Path())
+	bodySchema := &schema.BodySchema{}
+	if useAnySchema {
+		bodySchema = tfschema.AnySchemaForVariableCollection(mod.Path())
+	} else {
+		var err error
+		bodySchema, err = tfschema.SchemaForVariables(variables, mod.Path())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	pathCtx := &decoder.PathContext{
+		Schema:           bodySchema,
+		ReferenceOrigins: make(reference.Origins, 0),
+		ReferenceTargets: make(reference.Targets, 0),
+		Files:            make(map[string]*hcl.File),
+	}
+
+	if len(bodySchema.Attributes) > 0 {
+		// Only validate if this is actually a module
+		// as we may come across standalone tfvars files
+		// for which we have no context.
+		pathCtx.Validators = varsValidators
+	}
+
+	for _, origin := range mod.VarsRefOrigins {
+		if ast.IsVarsFilename(origin.OriginRange().Filename) {
+			pathCtx.ReferenceOrigins = append(pathCtx.ReferenceOrigins, origin)
+		}
+	}
+
+	for name, f := range mod.ParsedVarsFiles {
+		pathCtx.Files[name.String()] = f
+	}
+
+	return pathCtx, nil
+}
---
+++ internal/features/variables/jobs/references.go
@@ -0,0 +1,67 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package jobs
+
+import (
+	"context"
+
+	"github.com/hashicorp/hcl-lang/decoder"
+	"github.com/hashicorp/hcl-lang/lang"
+	idecoder "github.com/hashicorp/terraform-ls/internal/decoder"
+	"github.com/hashicorp/terraform-ls/internal/document"
+	fdecoder "github.com/hashicorp/terraform-ls/internal/features/variables/decoder"
+	"github.com/hashicorp/terraform-ls/internal/features/variables/state"
+	"github.com/hashicorp/terraform-ls/internal/job"
+	ilsp "github.com/hashicorp/terraform-ls/internal/lsp"
+	op "github.com/hashicorp/terraform-ls/internal/terraform/module/operation"
+)
+
+// DecodeVarsReferences collects reference origins within
+// variable files (*.tfvars) where each valid attribute
+// (as informed by schema provided via [LoadModuleMetadata])
+// is considered an origin.
+//
+// This is useful in hovering over those variable names,
+// go-to-definition and go-to-references.
+func DecodeVarsReferences(ctx context.Context, varStore *state.VariableStore, moduleFeature fdecoder.ModuleReader, modPath string) error {
+	mod, err := varStore.VariableRecordByPath(modPath)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Avoid collection if upstream (parsing) job reported no changes
+
+	// Avoid collection if it is already in progress or already done
+	if mod.VarsRefOriginsState != op.OpStateUnknown && !job.IgnoreState(ctx) {
+		return job.StateNotChangedErr{Dir: document.DirHandleFromPath(modPath)}
+	}
+
+	err = varStore.SetVarsReferenceOriginsState(modPath, op.OpStateLoading)
+	if err != nil {
+		return err
+	}
+
+	d := decoder.NewDecoder(&fdecoder.PathReader{
+		StateReader:  varStore,
+		ModuleReader: moduleFeature,
+		UseAnySchema: true,
+	})
+	d.SetContext(idecoder.DecoderContext(ctx))
+
+	varsDecoder, err := d.Path(lang.Path{
+		Path:       modPath,
+		LanguageID: ilsp.Tfvars.String(),
+	})
+	if err != nil {
+		return err
+	}
+
+	origins, rErr := varsDecoder.CollectReferenceOrigins()
+	sErr := varStore.UpdateVarsReferenceOrigins(modPath, origins, rErr)
+	if sErr != nil {
+		return sErr
+	}
+
+	return rErr
+}
