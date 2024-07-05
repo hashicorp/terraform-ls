588097c73da6eab3064444f3dee809dcf250a32f
Daniel Banck <daniel@dbanck.de>
2024-07-05 16:01:50 +0200

    Implement support for Terraform Stacks
    This commit adds initial support for Terraform Stacks files and Deployment files. This provides completion, hover, and diagnostics for Terraform Stacks and Deployment files.
    
    Co-authored-by: James Pogran <jpogran@outlook.com>

---
+++ internal/features/stacks/decoder/path_reader.go
@@ -0,0 +1,133 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package decoder
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/hcl-lang/decoder"
+	"github.com/hashicorp/hcl-lang/lang"
+	"github.com/hashicorp/hcl-lang/reference"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/terraform-ls/internal/features/stacks/ast"
+	"github.com/hashicorp/terraform-ls/internal/features/stacks/state"
+	ilsp "github.com/hashicorp/terraform-ls/internal/lsp"
+	stackschema "github.com/hashicorp/terraform-schema/schema"
+)
+
+type PathReader struct {
+	StateReader StateReader
+}
+
+type StateReader interface {
+	List() ([]*state.StackRecord, error)
+	StackRecordByPath(modPath string) (*state.StackRecord, error)
+}
+
+// PathContext returns a PathContext for the given path based on the language ID
+func (pr *PathReader) PathContext(path lang.Path) (*decoder.PathContext, error) {
+	record, err := pr.StateReader.StackRecordByPath(path.Path)
+	if err != nil {
+		return nil, err
+	}
+
+	switch path.LanguageID {
+	case ilsp.Stacks.String():
+		return stackPathContext(record)
+	case ilsp.Deploy.String():
+		return deployPathContext(record)
+	}
+
+	return nil, fmt.Errorf("unknown language ID: %q", path.LanguageID)
+}
+
+func stackPathContext(record *state.StackRecord) (*decoder.PathContext, error) {
+	// TODO: get Terraform version from record and use that to get the schema
+	// TODO: this should only work for terraform 1.8 and above
+	schema, err := stackschema.CoreStackSchemaForVersion(stackschema.LatestAvailableVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	pathCtx := &decoder.PathContext{
+		Schema:           schema,
+		ReferenceOrigins: make(reference.Origins, 0),
+		ReferenceTargets: make(reference.Targets, 0),
+		Files:            make(map[string]*hcl.File, 0),
+	}
+
+	// TODO: Add reference origins and targets if needed
+
+	for name, f := range record.ParsedFiles {
+		if _, ok := name.(ast.StackFilename); ok {
+			pathCtx.Files[name.String()] = f
+		}
+	}
+
+	return pathCtx, nil
+}
+
+func deployPathContext(record *state.StackRecord) (*decoder.PathContext, error) {
+	// TODO: get Terraform version from record and use that to get the schema
+	// TODO: this should only work for terraform 1.8 and above
+	schema, err := stackschema.CoreDeploySchemaForVersion(stackschema.LatestAvailableVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	pathCtx := &decoder.PathContext{
+		Schema:           schema,
+		ReferenceOrigins: make(reference.Origins, 0),
+		ReferenceTargets: make(reference.Targets, 0),
+		Files:            make(map[string]*hcl.File, 0),
+	}
+
+	// TODO: Add reference origins and targets if needed
+
+	for name, f := range record.ParsedFiles {
+		if _, ok := name.(ast.DeployFilename); ok {
+			pathCtx.Files[name.String()] = f
+		}
+	}
+
+	return pathCtx, nil
+}
+
+func (pr *PathReader) Paths(ctx context.Context) []lang.Path {
+	paths := make([]lang.Path, 0)
+
+	stackRecords, err := pr.StateReader.List()
+	if err != nil {
+		return paths
+	}
+
+	for _, record := range stackRecords {
+		foundStack := false
+		foundDeploy := false
+		for name := range record.ParsedFiles {
+			if _, ok := name.(ast.StackFilename); ok {
+				foundStack = true
+			}
+			if _, ok := name.(ast.DeployFilename); ok {
+				foundDeploy = true
+			}
+		}
+
+		if foundStack {
+			paths = append(paths, lang.Path{
+				Path:       record.Path(),
+				LanguageID: ilsp.Stacks.String(),
+			})
+		}
+		if foundDeploy {
+			paths = append(paths, lang.Path{
+				Path:       record.Path(),
+				LanguageID: ilsp.Deploy.String(),
+			})
+		}
+	}
+
+	return paths
+}
---
+++ internal/features/stacks/state/schema.go
@@ -0,0 +1,47 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package state
+
+import (
+	"io"
+	"log"
+
+	"github.com/hashicorp/go-memdb"
+	globalState "github.com/hashicorp/terraform-ls/internal/state"
+)
+
+const (
+	stackTableName = "stacks"
+)
+
+var dbSchema = &memdb.DBSchema{
+	Tables: map[string]*memdb.TableSchema{
+		stackTableName: {
+			Name: stackTableName,
+			Indexes: map[string]*memdb.IndexSchema{
+				"id": {
+					Name:    "id",
+					Unique:  true,
+					Indexer: &memdb.StringFieldIndex{Field: "path"},
+				},
+			},
+		},
+	},
+}
+
+func NewStackStore(changeStore *globalState.ChangeStore) (*StackStore, error) {
+	db, err := memdb.NewMemDB(dbSchema)
+	if err != nil {
+		return nil, err
+	}
+
+	discardLogger := log.New(io.Discard, "", 0)
+
+	return &StackStore{
+		db:          db,
+		tableName:   stackTableName,
+		logger:      discardLogger,
+		changeStore: changeStore,
+	}, nil
+}
