b7674b8c2777139fd4da52187637b6e29137b741
Ansgar Mertens <ansgar@hashicorp.com>
2024-08-08 11:15:33 +0200

    fix: fix panic on blocks with no labels during validation of label name (#1791)
    * fix: fix panic on blocks with no labels during validation of label name
    
    * chore: add changie entry

---
+++ internal/features/stacks/decoder/validations/valid_name_test.go
@@ -0,0 +1,20 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package validations
+
+import (
+	"testing"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+)
+
+func TestHasValidNameLabel_no_label(t *testing.T) {
+	var diags hcl.Diagnostics
+	block := hclsyntax.Block{
+		Labels: []string{}, // should not panic
+	}
+
+	hasValidNameLabel(&block, diags)
+}
